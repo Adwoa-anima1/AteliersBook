72b8b92a022d3e11e8ca76e0a7654389aa370bec
TheSistersGroup <madwoanima@gmail.com>
2026-08-02 11:02:11 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='ateliersbook-v14';
+const CACHE='ateliersbook-v15';
 const FILES=['./','./index.html','./extensions.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
